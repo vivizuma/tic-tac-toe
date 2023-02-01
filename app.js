8cea914f8a1c07d02e113c64d4471ce3284938cb
vivizuma <vitomanz@hotmail.co.uk>
2023-02-01 11:07:57 +0000

    ..

--- app.js
+++ app.js
@@ -32,9 +32,37 @@ const gameBoard = (() => {
   // () immediately invokes the function and returns grid and hello
 })();
 const gameController = (()=> {
+  let rounds = 0;
+  let gameOver = false;
   
+  const playRound = () => {
+    // put player marker on relevant grid
+    //check for win 
+    // if no win 
+    //increase rounds +1
+    //swap player turn
+    //update screen display
+
+    //if win. screencontroller.textstatus -> "currentPlayer wins!"
+  }
+  const checkWin = () =>{
+    let grid = gameBoard.grid;
+    console.log(grid)
+
+    //horizontal win conditions
+   for (i=0;i<3;i++){
+    console.log(grid)
+   }
+  }
+
+  return {checkWin}
 })
 
 const screenController = (() => {
-  
+  const updateGridView = () => {
+    let grid = getElementById('gameContainer');
+    grid.forEach
+  }
 })
+gameController().checkWin()
+console.log(gameBoard.grid)
